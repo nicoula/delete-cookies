dd3ae6d664702b1c3c93a5779b3b9dc418319b95
nicoula <nicoula@gmail.Com>
2016-12-08 22:26:19 +0800

    Remove useless commented code

--- background.js
+++ background.js
@@ -1,39 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// Thanks to https://chromium.googlesource.com/chromium/src/+/master/chrome/common/extensions/docs/examples/api/cookies/
-
-/* chrome.browserAction.onClicked.addListener(function(tab) {
-	var NbCookiesDeleted = 0;
-	var totalCookies = 0;
-	var savedCookies = [];
-	chrome.storage.sync.get('myCookies', function(result) {
-		if (!chrome.runtime.error && !chrome.runtime.lastError && typeof result.myCookies != "undefined") {
-			savedCookies = result.myCookies;
-			
-			savedCookies.forEach(function (myDomain) {
-				chrome.cookies.getAll({domain: myDomain}, function(cookies) {
-					for(var i=0; i<cookies.length;i++) {
-						totalCookies++;
-						var url = "http" + (cookies[i].secure ? "s" : "") + "://" + cookies[i].domain + cookies[i].path;
-						chrome.cookies.remove({"url": url + cookies[i].path, "name": cookies[i].name}, function(deleteCookies) {
-							NbCookiesDeleted++;
-							if (NbCookiesDeleted == totalCookies) {
-								chrome.runtime.sendMessage({todo: "deleteCookies"}, function(response) {
-									console.log("message received:");
-								});
-							}
-						});
-					}
-				});					
-			});
-		}
-	});
-}); */
-
-		
-var lol = 0;
 /* When the browser-action button is clicked... */
 chrome.browserAction.onClicked.addListener(function(tab) {
 	console.log("going to send a message bg.js");
